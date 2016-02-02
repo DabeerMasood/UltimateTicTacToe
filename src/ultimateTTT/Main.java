4cbf743524072779ae59a9ef5ebee9d981f28a76
Dabeer M. Masood <dabeermasood@gmail.com>
2016-02-01 22:20:53 -0600

    Indentation
    fixed indentation on code

--- src/ultimateTTT/Main.java
+++ src/ultimateTTT/Main.java
@@ -9,6 +9,8 @@ public class Main {
 		int littleBoardChoice=0;
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
+		
+		
 		System.out.println("Player one please input the Bigboard number followed by the littelboard number you want to play at");
 		System.out.println("Big Board Number:");
 		bigBoardChoice = scan.nextInt();
@@ -18,29 +20,34 @@ public class Main {
 		bigBoard.writeGuess(current,bigBoardChoice,littleBoardChoice);
 		int nextBoard= littleBoardChoice;
 		int turn =2;
+		
+		
 		while(bigBoard.checkWin(current)==false){
-		if(turn=2){
-		current = Move.O;
-		}else if(turn==1){
-		current = Move.X;
+			
+				
+				
+			if(turn==2){
+				current = Move.O;
+			}else if(turn==1){
+				current = Move.X;
+			}
+			if(bigBoardChoice==-1){
+				System.out.println("You may play anywhere else");
+				System.out.println("Pick a new BigBoard spot");
+				
+				bigBoardChoice = scan.nextInt();
+				}
+				
+			System.out.println("Player you are playing at " + bigBoardChoice);
+			System.out.println("Player input the little board number you want to play at");
+			System.out.println("Little Board Number:");
+			littleBoardChoice=scan.nextInt();
+			bigBoard.writeGuess(current,bigBoardChoice,littleBoardChoice);
+			
 		
-		if(nextBoard==-1){
-		System.out.println("You may play anywhere else");
-		System.out.println("Pick a new BigBoard spot");
-		
-		bigBoardChoice = scan.nextInt();
+			
+			
 		}
-		
-		System.out.println("Player you are playing at " + nextBoard);
-		System.out.println("Player input the little board number you want to play at");
-		System.out.println("Little Board Number:");
-		littleBoardChoice=scan.nextInt();
-		bigBoard.writeGuess(current,bigBoardChoice,littleBoardChoice);
-		
-		
-			
-			
-			
 			
 			
 		}
